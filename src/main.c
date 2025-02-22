4e982956a62d663285c02a9c985ae47cc4ca2ffb
deonnAlmirol <dalmirol@ucsd.edu>
2025-02-21 21:35:10 -0800

    added changes to main c file and help textfile

--- src/main.c
+++ src/main.c
@@ -39,9 +39,9 @@ int main(int argc, char* argv[]) {
         printf("Level %d\n", player_data->level);
     }
 
-    intro_text();
+    //intro_text();
     write_text("For a complete list of commands, type \"help\"\n", 45, 0);
-    main_loop();
+    main_loop(player_data);
 
     return 0;
 }
